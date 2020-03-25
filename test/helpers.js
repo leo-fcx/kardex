9659c6e713714ed47e43ca5ca92952eab36f0fe9
Leo <leo.fcx@gmail.com>
2020-03-25 04:53:01 -0400

    Adding Artillery tests

---
+++ test/helpers.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  generateRandomRecord
+};
+
+// Make sure to "npm install faker" first.
+const Faker = require('faker');
+
+function generateRandomRecord(userContext, events, done) {
+  // generate data with Faker:
+  const productId = Faker.random.uuid();
+  const quantity = Math.floor(Math.random() * 100) + 1;
+  const unitPrice = Math.floor(Math.random() * 100) + 90;
+  const price = unitPrice * quantity;
+  // add variables to virtual user's context:
+  userContext.vars.productId = productId;
+  userContext.vars.quantity = quantity;
+  userContext.vars.price = price;
+  // continue with executing the scenario:
+  return done();
+}
